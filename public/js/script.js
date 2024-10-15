6960fa00d599631b764e29e072dd0fb0195907cd
MOENNER QUENTIN <qmoenner@etudiant.univ-rennes1.fr>
2024-10-15 11:03:11 +0200

    create js script produit

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+let currentImageIndex = 0;
+const images = [
+    "public/img/tapis-de-course.jpeg",
+    "public/img/tapis-de-course-2.jpeg",
+    "public/img/tapis-de-course-3.jpeg",
+    "public/img/tapis-de-course-4.jpeg",
+    "public/img/tapis-de-course-5.jpeg"
+];
+
+function changeImage(direction) {
+    currentImageIndex += direction;
+
+    // Boucle autour des images
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    } else if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+
+    document.getElementById('mainImage').src = images[currentImageIndex];
+}
+
+function setImage(src) {
+    currentImageIndex = images.indexOf(src);
+    document.getElementById('mainImage').src = src;
+}
